56bb79ff5d9c5e2aa5446d5883d549c093fd59ef
byh0215 <852924899@qq.com>
2017-04-07 13:29:55 +0800

    test5:789qwe[]\zxc

--- src/answer/Resolve.java
+++ src/answer/Resolve.java
@@ -26,7 +26,7 @@ public class Resolve {
 		        // TODO Auto-generated method stub  
 		        Scanner s = new Scanner(System.in);  
 		        String str =s.next();
-                 //test:4 abcqwertyuijkl
+                 //test:5 789qwe[]\zxc
 		        System.out.println(lengthOfLongestSubstring(str));  
 		    }  
 }
