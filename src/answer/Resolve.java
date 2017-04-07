e1fcb405549e975661e0cdd4672f9a5340ebc32b
byh0215 <852924899@qq.com>
2017-04-07 13:21:04 +0800

    test1:aaaa

---
+++ src/answer/Resolve.java
@@ -0,0 +1,32 @@
+package answer;
+
+import java.util.*;
+
+public class Resolve {
+		    public static int lengthOfLongestSubstring(String s) {  
+		        Map<Character,Integer> map=new HashMap <Character,Integer>();  
+		        int maxLength=0;  
+		        int now=0;  
+		        for(int i=0;i<s.length();i++){  
+		            if(map.containsKey(s.charAt(i))){  
+		                now=Math.max(map.get(s.charAt(i))+1,now);  
+		                if((i-now+1)>maxLength){  
+		                    maxLength=i-now+1;    
+		                }  
+		            }else{  
+		                if((i-now+1)>maxLength){  
+		                    maxLength=i-now+1;  
+		                }  
+		            }  
+		            map.put(s.charAt(i), i);  
+		        }  
+		        return maxLength;  
+		    }  
+		    public static void main(String[] args) {  
+		        // TODO Auto-generated method stub  
+		        Scanner s = new Scanner(System.in);  
+		        String str =s.next();
+		        System.out.println(lengthOfLongestSubstring(str));  
+		    }  
+}
+
